91f0b53fcf459b62f9861ba4e3c2ba474f8bdc92
yaskh <khatri.yasir@gmail.com>
2020-03-01 13:54:54 +0500

    hour glass sum solved

--- MinimunSwaps/solution.py
+++ MinimunSwaps/solution.py
@@ -1,43 +1,34 @@
 
-def minimumSwaps1(arr):
-    count = 0
-    for i in range(len(arr)):
-        if arr[i] != i+1 :
-            temp = arr[i]
-            arr[temp - 1] = temp
-            arr[i] = i + 1
-            count += 1
-    for i in range(len(arr)):
-        try:
-            if arr[i]>arr[i+1]:
-                print(False)
-        except:
-            print(i)
-    return count
+
 
 def minimumSwaps(arr):
-    numSwaps = 0
+    count_swaps = 0
     i = 0
-    while(i < len(arr)-1):
-        if arr[i] != i+1:
-            tmp = arr[i]
-            arr[i] = arr[tmp-1]
-            arr[tmp - 1] = arr[i]
-            numSwaps += 1
-            pass
+    while(i<len(arr)-1):
+        if arr[i] != i + 1:
+            temp = arr[i]
+            arr[i],arr[temp - 1] = arr[temp - 1],arr[i]
+            count_swaps += 1
         else:
             i += 1
-    return numSwaps
+    return count_swaps
 
-# with open("input.txt","r") as fp:
-#     data = fp.read()
-#
-# data = data.split("\n")
-#
-# data = data[1].split(" ")
-# data = [int(i) for i in data]
-#
-# print(minimumSwaps(data))
+
+
+
+
+
+
+
+with open("input.txt","r") as fp:
+    data = fp.read()
+
+data = data.split("\n")
+
+data = data[1].split(" ")
+data = [int(i) for i in data]
+
+print(minimumSwaps(data))
 
 arr= [4, 3, 1, 2]
 print(minimumSwaps(arr))
